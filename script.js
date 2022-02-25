dc34e1d143d6a9040c44fea196b7a87d49003a64
Nodosy <nodar.rusia@gmail.com>
2022-02-25 16:34:46 +0400

    chore: add code

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
-let animal = {};
-let cat = {};
+// Given the string 'ahb acb aeb aeeb adcb axeb'. Write a regular expression that matches the strings ahb, acb, aeb by pattern: letter 'a', any character, letter 'b'
+let str1 = "ahb acb aeb aeeb adcb axeb";
 
-animal.move = function () {
-  console.log("moving...");
-};
-cat.__proto__ = animal;
+// Given the string '2 + 3 223 2223'. Write a regex that finds line 2 + 3 without capturing the rest
+let str2 = "2 + 3 223 2223";
+
+// Get the day, month and year of the current date and output it to the console separately
+let currentDate = new Date();
+let day = currentDate.getDay();
+let month = currentDate.getMonth();
+let year = currentDate.getFullYear();
+console.log(day);
+console.log(month);
+console.log(year);
