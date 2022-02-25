5d2153355a238a188e4e6566118bd55cd48ca755
Nodosy <nodar.rusia@gmail.com>
2022-02-25 14:45:22 +0400

    chore add code

--- script.js
+++ script.js
@@ -1,50 +1,78 @@
-// Create a car object, add a color property to it with the value equals 'black'
-let car = {};
-car.color = "black";
-console.log("color: ", car.color);
+// Given an array consisting of movie names, iterate over the array with the output of the names of each movie to the console
+let movies = [
+  "One Flew Over the Cuckoo's Nest",
+  "Apocalypse Now",
+  "Taxi Driver",
+  "A Clockwork Orange",
+  "Forrest Gump",
+  "No Country for Old Men",
+  "Schindler's List",
+  "Once Upon a Time in America",
+  "Pulp Fiction",
+  "Twelve Monkeys",
+  "Se7en",
+  "The Godfather",
+];
+let name = movies.map((movie) => {
+  console.log(movie);
+});
 
-// Change the color property of the car object to 'green'
-car.color = "green";
-console.log("color: ", car.color);
+// Given an array of car manufacturers, convert the array to a string and back to an array
+let manufacturers = ["Tesla", "Mazda", "BMW", "Subaru", "Porsche"];
+let manuToString = manufacturers.toString();
+let manuToArray = manuToString.split(",");
 
-// Add the power property to the car object, which is a function and displays the engine power to the console
-car.power = function (power) {
-  console.log(power);
+// Given an array of the names of your friends, add the words hello to each element of the array
+let friends = ["Jack", "John", "Jerry", "Linda", "Marry"];
+let helloFriends = friends.map((friend) => {
+  return `Hello ${friend}`;
+});
+
+// Convert numeric array to Boolean
+let numerics = [1, 2, 3, 0, -99];
+let boolnumerics = numerics.map((numeric) => {
+  return Boolean(numeric);
+});
+
+// Sort the array [1,6,7,8,3,4,5,6] in descending order
+let orderedArray = [1, 6, 7, 8, 3, 4, 5, 6];
+let sortedArray = orderedArray.sort();
+let descendingArray = sortedArray.reverse();
+
+// Filter array [1,6,7,8,3,4,5,6] by value> 3
+let filterArr = [1, 6, 7, 8, 3, 4, 5, 6];
+let filteredArr = filterArr.filter((value) => {
+  return value > 3;
+});
+
+// Write a function that takes two parameters - an array and a number and outputs the index of an array element equal to a number
+const indexElement = function (arr, ind) {
+  return arr[ind];
 };
-console.log(car.power(199));
 
-// Pears and apples are accepted to the warehouse, write a function that returns the result of adding the number of accepted pears and apples
-let countFruits = function (pearsNumber, applesNumber) {
-  return pearsNumber + applesNumber;
-};
-let acceptedPearsApples = countFruits(10, 20);
-console.log(acceptedPearsApples);
-
-// Your name is saved in the payment terminal, write a function to define the name in the terminal (if you entered your name, then hello + name, if not, then there is no such name)
-const NAME = "Nodari";
-if (NAME === "Nodari") {
-  console.log(`Hello ${NAME}`);
-} else {
-  console.log("There is no such name.");
+// Implement a loop that will print the number 'a' until it is less than 10
+for (let a = 0; a < 10; a++) {
+  console.log(a);
 }
 
-// Write a function for calculating the type of argument and type output to the console
-let arguments = function (argument) {
-  console.log(typeof argument);
-};
-arguments("hi");
-arguments(false);
-
-// Write a function that determines whether a number is prime or not
+// Implement a loop that prints prime numbers to the console
 let primeNumber = function (number) {
   for (let i = 2; i * i <= number; i++) {
     if (number % i === 0) {
-      return false;
+      console.log("not prime");
+    } else {
+      console.log("prime number");
     }
   }
-  return number > 1;
 };
 let num1 = primeNumber(4);
-let num2 = primeNumber(5);
-console.log(num1);
-console.log(num2);
+
+// Implement a loop that prints odd numbers to the console
+let oddNumber = function (number) {
+  if (number % 2 === 0) {
+    console.log("even number");
+  } else {
+    console.log("odd number");
+  }
+};
+let num2 = oddNumber(8);
