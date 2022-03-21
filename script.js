065fbdcb34648b6837493fec531a4c26bfb2443a
Nodosy <nodar.rusia@gmail.com>
2022-03-21 12:59:31 +0400

    chore: fix errors in code

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ let movies = [
   "Se7en",
   "The Godfather",
 ];
-let name = movies.map((movie) => {
-  console.log(movie);
+movies.forEach(function (element) {
+  console.log(element);
 });
 
 // Given an array of car manufacturers, convert the array to a string and back to an array
@@ -34,10 +34,10 @@ let boolnumerics = numerics.map((numeric) => {
   return Boolean(numeric);
 });
 
-// Sort the array [1,6,7,8,3,4,5,6] in descending order
-let orderedArray = [1, 6, 7, 8, 3, 4, 5, 6];
-let sortedArray = orderedArray.sort();
-let descendingArray = sortedArray.reverse();
+// Sort the array [9, 10, 7, 21, 2, 1, 6, 7, 8, 3, 4, 11, 5, 6] in descending order
+let array_of_numbers = [9, 10, 7, 21, 2, 1, 6, 7, 8, 3, 4, 11, 5, 6];
+array_of_numbers.sort((a, b) => b - a);
+console.log(array_of_numbers);
 
 // Filter array [1,6,7,8,3,4,5,6] by value> 3
 let filterArr = [1, 6, 7, 8, 3, 4, 5, 6];
@@ -46,9 +46,25 @@ let filteredArr = filterArr.filter((value) => {
 });
 
 // Write a function that takes two parameters - an array and a number and outputs the index of an array element equal to a number
-const indexElement = function (arr, ind) {
-  return arr[ind];
-};
+function my_findIndex(array, callback) {
+  const length = array.length;
+  for (let index = 0; index < length; index += 1) {
+    const value = array[index];
+    if (callback(value, index, array)) {
+      return index;
+    }
+  }
+  return -1;
+}
+function my_indexOf(array, searchedValue) {
+  return my_findIndex(array, function (value) {
+    return value === searchedValue;
+  });
+}
+const my_array_one = [0, 11, 2, 34];
+const my_array_two = [14, 0, 2, 34];
+console.log("index of 0 is: ", my_indexOf(my_array_one, 0));
+console.log("index of 0 is: ", my_indexOf(my_array_two, 0));
 
 // Implement a loop that will print the number 'a' until it is less than 10
 for (let a = 0; a < 10; a++) {
@@ -75,4 +91,4 @@ let oddNumber = function (number) {
     console.log("odd number");
   }
 };
-let num2 = oddNumber(8);
+oddNumber(8);
