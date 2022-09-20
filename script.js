e951b77bdecae3e56e951f8be4e6b901edf02a36
Nodosy <nodar.rusia@gmail.com>
2022-09-20 13:18:02 +0400

    test: add code review fixes

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-// Create a calculator class with the following methods:
-// add - this method should summarize the parameters passed to it and return the sum as a result
-// multiply - this method should multiply the parameters passed to it and return the product as a result
-// Create unit tests that verify that the critical calculator's methods are working correctly.
-// Create a coverage reporter and take a screenshot, commit it to the homework repository.
 class Calculator {
   constructor(...numbers) {
     this.numbers = numbers;
   }
   add() {
-    var sum = 0;
+    let sum = 0;
     for (const num of this.numbers) {
       if (typeof num === "number" && Number.isNaN(num) === false) {
         sum += num;
@@ -19,7 +14,7 @@ class Calculator {
     return sum;
   }
   multiply() {
-    var product = 1;
+    let product = 1;
     for (const num of this.numbers) {
       if (typeof num === "number" && Number.isNaN(num) === false) {
         product *= num;
@@ -30,7 +25,5 @@ class Calculator {
     return product;
   }
 }
-// const calc1 = new Calculator(101, -1, 0, undefined);
-// console.log(calc1.add());
 
 module.exports = Calculator;
--- test/calculatorMethods.spec.js
+++ test/calculatorMethods.spec.js
@@ -3,68 +3,84 @@ let expect = chai.expect;
 let Calculator = require("../script.js");
 
 describe("Calculator", () => {
-  it("should work addition", () => {
-    let calculator = new Calculator(3, 4, 5);
-    expect(calculator.add()).to.equal(12);
-
-    let calculator2 = new Calculator(-3, -4, -5);
-    expect(calculator2.add()).to.equal(-12);
-
-    let calculator_3 = new Calculator(0, 1);
-    expect(calculator_3.add()).to.equal(1);
-
-    let calculator_4 = new Calculator(-1, 0);
-    expect(calculator_4.add()).to.equal(-1);
-
-    let calculator_5 = new Calculator(-1, 3);
-    expect(calculator_5.add()).to.equal(2);
-
-    let calculator_6 = new Calculator(101, -1, 0, undefined);
-    expect(function () {
-      calculator_6.add();
-    }).to.throw();
-
-    let calculator_7 = new Calculator(101, -1, 0, null);
-    expect(function () {
-      calculator_7.add();
-    }).to.throw();
-
-    let calculator_8 = new Calculator(101, -1, 0, "I am not a number");
-    expect(function () {
-      calculator_8.add();
-    }).to.throw();
+  describe("Addition", () => {
+    it("should add three positive numbers", () => {
+      let calculator0 = new Calculator(3, 4, 5);
+      expect(calculator0.add()).to.equal(12);
+    });
+    it("should add three negative numbers", () => {
+      let calculator1 = new Calculator(-3, -4, -5);
+      expect(calculator1.add()).to.equal(-12);
+    });
+    it("should add 0 and 1", () => {
+      let calculator2 = new Calculator(0, 1);
+      expect(calculator2.add()).to.equal(1);
+    });
+    it("should add -1 and 0", () => {
+      let calculator3 = new Calculator(-1, 0);
+      expect(calculator3.add()).to.equal(-1);
+    });
+    it("should add -1 and 3", () => {
+      let calculator4 = new Calculator(-1, 3);
+      expect(calculator4.add()).to.equal(2);
+    });
+    it("should not add undefined", () => {
+      let calculator5 = new Calculator(101, -1, 0, undefined);
+      expect(function () {
+        calculator5.add();
+      }).to.throw();
+    });
+    it("should not add null", () => {
+      let calculator6 = new Calculator(101, -1, 0, null);
+      expect(function () {
+        calculator6.add();
+      }).to.throw();
+    });
+    it("should not add string", () => {
+      let calculator7 = new Calculator(101, -1, 0, "I am not a number");
+      expect(function () {
+        calculator7.add();
+      }).to.throw();
+    });
   });
-  it("should work multiply", () => {
-    let calculator = new Calculator(3, 4, 5);
-    expect(calculator.multiply()).to.equal(60);
-
-    let calculator2 = new Calculator(-3, -4);
-    expect(calculator2.multiply()).to.equal(12);
-
-    let calculator_3 = new Calculator(0, 1);
-    expect(calculator_3.multiply()).to.equal(0);
-
-    let calculator_4 = new Calculator(-1, 0);
-    expect(calculator_4.multiply()).to.equal(0);
-
-    let calculator_5 = new Calculator(-2, 3);
-    expect(calculator_5.multiply()).to.equal(-6);
-
-    let calculator_6 = new Calculator(101, -1, 0, undefined);
-    expect(function () {
-      calculator_6.multiply();
-    }).to.throw();
-
-    let calculator_7 = new Calculator(101, -1, 0, null);
-    expect(function () {
-      calculator_7.multiply();
-    }).to.throw();
-
-    let calculator_8 = new Calculator(101, -1, 0, "I am not a number");
-    expect(function () {
-      calculator_8.multiply();
-    }).to.throw();
+  describe("Multiplication", () => {
+    it("should multiply three positive numbers", () => {
+      let calculator0 = new Calculator(3, 4, 5);
+      expect(calculator0.multiply()).to.equal(60);
+    });
+    it("should multipkly two negative numbers", () => {
+      let calculator1 = new Calculator(-3, -4);
+      expect(calculator1.multiply()).to.equal(12);
+    });
+    it("should multiply 0 and 1", () => {
+      let calculator2 = new Calculator(0, 1);
+      expect(calculator2.multiply()).to.equal(0);
+    });
+    it("should multiply -1 and 0", () => {
+      let calculator3 = new Calculator(-1, 0);
+      expect(calculator3.multiply()).to.equal(0);
+    });
+    it("should multiply -2 and 3", () => {
+      let calculator4 = new Calculator(-2, 3);
+      expect(calculator4.multiply()).to.equal(-6);
+    });
+    it("should not multiply undefined", () => {
+      let calculator5 = new Calculator(101, -1, 0, undefined);
+      expect(function () {
+        calculator5.multiply();
+      }).to.throw();
+    });
+    it("should not multiply null", () => {
+      let calculator6 = new Calculator(101, -1, 0, null);
+      expect(function () {
+        calculator6.multiply();
+      }).to.throw();
+    });
+    it("should not multiply string", () => {
+      let calculator7 = new Calculator(101, -1, 0, "I am not a number");
+      expect(function () {
+        calculator7.multiply();
+      }).to.throw();
+    });
   });
 });
-
-// npm test
